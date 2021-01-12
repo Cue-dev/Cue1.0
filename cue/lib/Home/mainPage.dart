efb8b263683677562ac247faa3eb87bc39278c63
jyw3927 <64010987+jyw3927@users.noreply.github.com>
2021-01-12 14:17:49 +0900

    1/12 commit
    updated mainpage.dart

--- cue/lib/Home/mainPage.dart
+++ cue/lib/Home/mainPage.dart
@@ -9,39 +9,76 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   int _selectedIndex = 0;
-  static List<Widget> _widgetOptions = <Widget>[
-    PlayListPage(),
-    Container(
-      child: Text('Search Page'),
-    ),
-    CloudStorageDemo(),
-    NotificationPage(),
-    Container(
-      child: Text('My Page'),
-    ),
-  ];
+  // static List<Widget> _widgetOptions = <Widget>[
+  //   PlayListPage(),
+  //   Container(
+  //     child: Text('Search Page'),
+  //   ),
+  //   CloudStorageDemo(),
+  //   NotificationPage(),
+  //   Container(
+  //     child: Text('My Page'),
+  //   ),
+  // ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _widgetOptions,
+      // body: IndexedStack(
+      //   index: _selectedIndex,
+      //   children: _widgetOptions,
+      // ),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          children: <Widget>[
+            PlayListPage(),
+            Container(
+              child: Text('Search Page'),
+            ),
+            CloudStorageDemo(),
+            NotificationPage(),
+            Container(
+              child: Text('My Page'),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        unselectedItemColor: Colors.grey[900],
+        // showSelectedLabels: false,
+        // showUnselectedLabels: false,
+        unselectedItemColor: Colors.black.withOpacity(.60),
         selectedItemColor: Colors.black,
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Color.fromRGBO(249, 249, 249, 1),
+        // backgroundColor: Color.fromRGBO(249, 249, 249, 1),
+        elevation: 5.0,
+        backgroundColor: Colors.white.withOpacity(.90),
         items: <BottomNavigationBarItem>[
-          _buildBottomNavigationBarItem(icon1: Icon(Icons.home)),
-          _buildBottomNavigationBarItem(icon1: Icon(Icons.search)),
-          _buildBottomNavigationBarItem(icon1: Icon(Icons.add_circle)),
-          _buildBottomNavigationBarItem(icon1: Icon(Icons.notifications)),
-          _buildBottomNavigationBarItem(icon1: Icon(Icons.person)),
+          BottomNavigationBarItem(label: '홈', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: '검색', icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: '구독', icon: Icon(Icons.menu)),
+          BottomNavigationBarItem(label: '알림', icon: Icon(Icons.notifications)),
+          BottomNavigationBarItem(label: '마이', icon: Icon(Icons.person)),
         ],
         currentIndex: _selectedIndex,
         onTap: (index) => _onItemTapped(index),
@@ -49,16 +86,11 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  BottomNavigationBarItem _buildBottomNavigationBarItem({Icon icon1}) {
-    return BottomNavigationBarItem(
-      icon: icon1,
-      title: Text(''),
-    );
-  }
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      _pageController.animateToPage(index,
+          duration: Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
     });
   }
 }
