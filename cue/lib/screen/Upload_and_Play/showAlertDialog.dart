d59e55aae7575e8081ae3a373c9e0b86e3fd9011
jyw3927 <64010987+jyw3927@users.noreply.github.com>
2021-01-20 09:55:43 +0900

    1/20

--- cue/lib/screen/Upload_and_Play/showAlertDialog.dart
+++ cue/lib/screen/Upload_and_Play/showAlertDialog.dart
@@ -6,21 +6,12 @@ class CheckboxesDemo extends StatefulWidget {
 }
 
 class _CheckboxesDemoState extends State<CheckboxesDemo> {
-  List<bool> checked= [false,false,false,false,false,false];
-  List<String> scraplist = [
-    '좋아하는 영화 명대사',
-    '딕션 연습',
-    '눈물 연기 연습',
-    '분노 연기 연습'
-  ];
+  List<bool> checked = [false, false, false, false, false, false];
+  List<String> scraplist = ['좋아하는 영화 명대사', '딕션 연습', '눈물 연기 연습', '분노 연기 연습'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Checkboxes Demo'),
-        backgroundColor: Color(0xFF6200EE),
-      ),
       body: Container(
         margin: EdgeInsets.symmetric(vertical: 40),
         child: Column(
@@ -31,16 +22,14 @@ class _CheckboxesDemoState extends State<CheckboxesDemo> {
                   Checkbox(
                     onChanged: (bool value) {
                       setState(() {
-                        checked[i] = value;
+                        checked[i] = !checked[i];
                       });
                     },
-
                     value: checked[i],
                     activeColor: Color(0xFF6200EE),
                   ),
                   Text(
                     scraplist[i],
-
                   ),
                 ],
                 mainAxisAlignment: MainAxisAlignment.center,
